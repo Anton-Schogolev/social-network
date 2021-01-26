793c2798b028008a4541980be445c08f53608420
Anton-Schogolev <Anton.Schogolev@gmail.com>
2021-01-26 11:46:04 +0300

    Pagination optimised

--- src/Components/Users/User/Users.tsx
+++ src/Components/Users/User/Users.tsx
@@ -15,22 +15,23 @@ type PropsType = {
 
 export const Users = (props: PropsType) => {
     const numbersOfPages: number[] = []
-    for (let i = props.currentPage - 4; i <= props.currentPage + 4 && i <= props.numberOfPages; i++) {
-        if (i < 1)
-            i = 1
+    for (let i = props.currentPage - 4; i <= props.currentPage + 4 && i < props.numberOfPages; i++) {
+        if (i < 2)
+            i = 2
         numbersOfPages.push(i)
     }
-    const Span = ({value, Class}: { value: number; Class?: boolean }) => <span
-        className={Class ? s.activePage : ""}
-        onClick={() => {props.changePage(value)}}>{value} </span>
-    const pages = <div>{numbersOfPages[0] >= 2 ?
-        <Span value={1}/> : ""}{numbersOfPages[0] <= 2 ? numbersOfPages.map(value =>
-        <Span value={value} Class={props.currentPage === value}/>) : (<>... {numbersOfPages.map(value =>
-        <Span value={value}
-              Class={props.currentPage === value}/>)}</>)} {numbersOfPages[numbersOfPages.length - 1] === props.numberOfPages ? "" : (
-        <>{numbersOfPages[numbersOfPages.length - 1] === props.numberOfPages-1 ? "" : "..."} <Span
-            value={props.numberOfPages}/></>
-    )}</div>;
+    const Span = ({value}: { value: number}) => <span
+        className={value === props.currentPage ? s.activePage : ""}
+        onClick={() => {
+            props.changePage(value)
+        }}>{value} </span>
+
+    const pages = <div>
+        <Span value={1}/> {numbersOfPages[0] !== 2 ? "... " : ""}{numbersOfPages.map(value =>
+        <Span value={value} />)}
+        {numbersOfPages[numbersOfPages.length - 1] !== props.numberOfPages - 1 ? " ... " : ""}
+        <Span value={props.numberOfPages}/>
+    </div>;
     return <>
         {pages}
         {props.users.map(us => <User key={us.id} user={us} follow={props.follow}
