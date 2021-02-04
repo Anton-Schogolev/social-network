35807d40e8b587f275dbb7ef1e710ac6af91d2ca
Anton <anton.schogolev@gmail.com>
2021-02-04 21:30:28 +0300

    bug fix

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,20 +1,16 @@
-import React, {useCallback} from "react";
+import React from "react";
 import {Post} from "./Post/Post";
 import NewPost from "./NewPost/NewPost";
 import {PostType} from "../../../types/entities";
 import {StateType} from "../../../redux/reduxStore";
-import {useDispatch, useSelector} from "react-redux";
-import {deletePostAC} from "../../../redux/profileReducer";
-
+import {useSelector} from "react-redux";
 
 
 const MyPosts: React.FC = () => {
     const posts = useSelector<StateType, Array<PostType>>(state => state.posts.postsArray)
-    const dispatch = useDispatch()
     const postDataMap = (x: PostType): JSX.Element => {
-        const deletePost = useCallback(() => dispatch(deletePostAC(x.id)),[x.id, dispatch])
         return (
-            <Post key={x.id} text={x.text} ava={x.ava} amountOfLikes={x.amountOfLikes} id={x.id} deletePost={deletePost}/>
+            <Post key={x.id} text={x.text} ava={x.ava} amountOfLikes={x.amountOfLikes} id={x.id}/>
         )
     }
     return (
--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, {useCallback} from "react";
 import s from "./Post.module.css"
 import {PostType} from "../../../../types/entities";
+import {deletePostAC} from "../../../../redux/profileReducer";
+import {useDispatch} from "react-redux";
 
-type PropsType = PostType & {
-    deletePost: () => void
-}
+type PropsType = PostType & {}
 
 export const Post = React.memo((props: PropsType) => {
+    const dispatch = useDispatch()
+
+    const deletePost = useCallback(() => dispatch(deletePostAC(props.id)),[props.id, dispatch])
     return (
         <div className={s.container}>
             <img src={props.ava} className={s.ava} alt={""}/>
             <div className={s.postText}>
                 <pre className={s.preText}>{props.text}</pre>
-                <button onClick={props.deletePost} className={s.delete}>x</button>
+                <button onClick={deletePost} className={s.delete}>x</button>
             </div>
             <p className={s.likes}>Like {props.amountOfLikes}</p>
         </div>
